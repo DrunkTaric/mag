eb9d562d291d6228eefd5130d4b94f719998901c
ComboRush <comborush.init@proton.me>
2024-01-06 06:03:19 +0200

    commands loader

---
+++ src/commander.ts
@@ -0,0 +1,26 @@
+import fs from "node:fs"
+import path from "node:path"
+import { RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord.js";
+
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+let commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+			console.log(`Registered command /${command.data.name}`);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+export default commands
