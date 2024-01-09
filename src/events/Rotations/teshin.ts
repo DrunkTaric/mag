5fed5d61636b58e84f8d32d95b1e1de1b9bfc8ec
ComboRush <comborush.init@proton.me>
2024-01-09 17:18:33 +0200

    teshin rotation

---
+++ src/events/Rotations/teshin.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import axios from 'axios';
+
+module.exports = {
+    name: 'Teshin',
+    alone: false,
+    api: async () => {
+        try {
+            const { data } = await axios.get("https://api.warframestat.us/pc/en/steelPath/")
+            if (data.error) { return "Umbra Forma" }
+            return data["currentReward"]["name"]
+        }catch(_) {
+            return "Umbra Forma"
+        }
+    },
+    time: 1000 * 60, //* 60 * 24,
+    rewards: [
+        `${process.env["UMBRA_FORMA"]} Umbra Forma ${process.env["STEEL_ESSENCE"]} 150`,
+        `${process.env["KUVA"]} 50,000 Kuva ${process.env["STEEL_ESSENCE"]} 55`,
+        `${process.env["RIVEN"]} Kitgun Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
+        `${process.env["FORMA"]} 3x Forma ${process.env["STEEL_ESSENCE"]} 75`,
+        `${process.env["RIVEN"]} Zaw Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
+        `${process.env["RIVEN"]} 30,000 Endo ${process.env["STEEL_ESSENCE"]} 150`,
+        `${process.env["RIVEN"]} Rifle Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
+        `${process.env["RIVEN"]} Shotgun Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
+    ],
+}
