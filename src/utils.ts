5eb544bf0620d406d562ed4408ef32b1d8b1f78e
ComboRush <comborush.init@proton.me>
2024-01-12 23:38:26 +0200

    some utilities

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+import { EmbedBuilder, Message, TextChannel } from "discord.js";
+
+export async function sendMessage(msg: string | EmbedBuilder, channel: TextChannel | undefined) {
+    if (msg instanceof EmbedBuilder) {
+        return await channel?.send({ embeds: [msg] })
+    }
+    return await channel?.send(msg)
+}
+
+export async function editMessage(msg: string | EmbedBuilder, message: Message<true> | undefined) {
+    if (msg instanceof EmbedBuilder) {
+        return await message?.edit({ embeds: [msg] })
+    }
+    return await message?.edit(msg)
+}
