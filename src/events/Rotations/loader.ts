435e7df5b79f182c796ea24f0f8a56ec91e5d5fd
ComboRush <comborush.init@proton.me>
2024-01-12 23:39:43 +0200

    import files

--- src/events/Rotations/loader.ts
+++ src/events/Rotations/loader.ts
@@ -6,34 +6,18 @@ import fs from "node:fs"
 
 export async function getRotations(channel: TextChannel | undefined) {
     let message = await channel?.send("Rotations");
+function PullFiles() {
+    let files = []
     const filesPath = path.join(__dirname);
-    const files = fs.readdirSync(filesPath);
-    let rotations: { name: string, value: string, inline: boolean }[] = []
-    for (const file of files) {
-        if (file == "loader.js") { continue; }
-        const filePath = path.join(filesPath, file);
-        let rotation = require(filePath);
-        if (rotation.alone) { continue; }
-        rotations.push({ name: rotation.name, value: `
-            Current:
-            __${await rotation.api()}__
-            Rewards:
-            ${rotation.rewards.join("\n")}
-        `, inline: true })
-        let job = corn.schedule(rotation.time, async () => {
-            console.log("Rotations: " + rotation.name)
-            rotations.map(async (rot) => { if(rot.name == rotation.name) rot.value = `
-                Current:
-                __${await rotation.api()}__
-                Rewards:
-                ${rotation.rewards.join("\n")}
-            ` });
-            await message?.edit({ embeds: [
-                new EmbedBuilder()
-                    .setColor(0x0099FF)
-                    .setTitle("Rotations")
-                    .addFields(rotations)
-            ]});
+    const file_names = fs.readdirSync(filesPath);
+    for (const file_name of file_names) {
+        if (file_name == "loader.js") { continue; }
+        const filePath = path.join(filesPath, file_name);
+        let file = require(filePath);
+        files.push(file)
+    }
+    return files
+}
         })
     }
     message?.edit({ embeds: [
