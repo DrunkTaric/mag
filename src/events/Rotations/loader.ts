0368debb0d73fffbe526149f9edb94522c7fcbe8
ComboRush <comborush.init@proton.me>
2024-01-14 23:31:27 +0200

    things

--- src/events/Rotations/loader.ts
+++ src/events/Rotations/loader.ts
@@ -9,18 +9,21 @@ function PullFiles() {
     let files = []
     const filesPath = path.join(__dirname);
     const file_names = fs.readdirSync(filesPath);
+
     for (const file_name of file_names) {
         if (file_name == "loader.js") { continue; }
         const filePath = path.join(filesPath, file_name);
         let file = require(filePath);
         files.push(file)
     }
+
     return files
 }
 
 function makeSchedules(schedules: { [key: string]: Schedule[] }, channel: TextChannel | undefined) {
     if (!schedules) { return; }
     if (!channel) { return; }
+
     for (const [key, value] of Object.entries(schedules)) {
         corn.schedule(key, async () => {
             for (const schedule of value) {
@@ -30,22 +33,19 @@ function makeSchedules(schedules: { [key: string]: Schedule[] }, channel: TextCh
             console.log("=================")
         })
     }
-
 }
 
-export async function getRotations(channel: TextChannel | undefined) {
+export async function makeRotations(channel: TextChannel | undefined) {
     let schedules: { [key: string]: Schedule[] } = {}
     const files = PullFiles();
 
     for (const rotation of files) {
-        let rotation_message = await sendMessage(new EmbedBuilder().setTitle(rotation.name).setColor(0x0099FF).addFields({ name: rotation.name, value: await rotation.api() }), channel);
+        let rotation_message = await sendMessage(new EmbedBuilder().setTitle(rotation.name).setColor(0x0099FF).addFields({ name: rotation.name, value: await rotation.api(rotation) }), channel);
         if (schedules[rotation.time] == undefined) {
             schedules[rotation.time] = []
         }
         schedules[rotation.time].push({ name: rotation.name, message: rotation_message, callback: rotation.api })
     }
-
-    console.log(JSON.stringify(schedules))
 
     makeSchedules(schedules, channel)
 }
