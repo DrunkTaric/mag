7dc2bac5c95eb51f8b1311f7347205665212055f
ComboRush <comborush.init@proton.me>
2024-01-14 23:30:31 +0200

    making structure message functon for the rewards displaying

--- src/utils.ts
+++ src/utils.ts
@@ -12,4 +12,27 @@ export async function editMessage(msg: string | EmbedBuilder, message: Message<t
         return await message?.edit({ embeds: [msg] })
     }
     return await message?.edit(msg)
+
+export function structureMessage(rewards: string[] | string[][], current: string) {
+    let message = "";
+    if (Array.isArray(rewards[0])) {
+        let arrayIndex = getIndex(rewards as string[][], current)
+        message += `**Current Week Rewards:**\n`
+        for (let i = 0; i < rewards[arrayIndex].length; i++) {
+            message += `${rewards[arrayIndex][i]}\n`
+        }
+        message += `**Next Week Rewards:**\n`
+        arrayIndex = rewards.length - 1 >= arrayIndex + 1? arrayIndex + 1 : 0
+        for (let i = 0; i < rewards[arrayIndex].length; i++) {
+            message += `${rewards[arrayIndex][i]}\n`
+        }
+        return message
+    }
+    message += `**Current:**\n`
+    message += `${rewards[getIndex(rewards, current)]}\n`
+    message += `**Rewards:**\n`
+    for (let i = 0; i < rewards.length; i++) {
+        message += `${rewards[i]}\n`
+    }
+    return message
 }
