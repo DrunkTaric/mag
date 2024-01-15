c4af8c8db1da6cc6dfbd08979e87e25665c24e21
ComboRush <comborush.init@proton.me>
2024-01-15 08:43:00 +0200

    a little but that i was looping in 1 dimisional array

--- utils.ts
+++ utils.ts
@@ -17,8 +17,10 @@ export async function editMessage(msg: string | EmbedBuilder, message: Message<t
 export function getIndex(list : string[][] | string[], current: string) {
     if (Array.isArray(list[0])) {
         for (let i = 0; i < list.length; i++) {
-            if (list[i].includes(current)) {
-                return i
+            for (let j = 0; j < list[i].length; j++) {
+                if (list[i][j].includes(current)) {
+                    return i
+                }   
             }
         }
     }
