66923c133bb9c4af0e73ec1c0103007b6f68db44
ComboRush <comborush.init@proton.me>
2024-01-15 08:42:16 +0200

    making it endo icon

--- events/Rotations/teshin.ts
+++ events/Rotations/teshin.ts
@@ -21,7 +21,7 @@ module.exports = {
         `${process.env["RIVEN"]} Kitgun Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
         `${process.env["FORMA"]} 3x Forma ${process.env["STEEL_ESSENCE"]} 75`,
         `${process.env["RIVEN"]} Zaw Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
-        `${process.env["RIVEN"]} 30,000 Endo ${process.env["STEEL_ESSENCE"]} 150`,
+        `${process.env["ENDO"]} 30,000 Endo ${process.env["STEEL_ESSENCE"]} 150`,
         `${process.env["RIVEN"]} Rifle Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
         `${process.env["RIVEN"]} Shotgun Riven Mod ${process.env["STEEL_ESSENCE"]} 75`,
     ],
