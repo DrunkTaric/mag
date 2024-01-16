08c6a42523f12cc63c04b0b617aa7694a2addfdf
ComboRush <comborush.init@proton.me>
2024-01-16 23:47:20 +0200

    small change

--- main.ts
+++ main.ts
@@ -10,7 +10,6 @@ if (!process.env.BOT_TOKEN) { throw new Error('Missing BOT_TOKEN in .env') }
 if (!process.env.SUPABASE_URL) { throw new Error('Missing SUPABASE_URL in .env') }
 if (!process.env.SUPABASE_ANON) { throw new Error('Missing SUPABASE_ANON in .env') }
 
-const rest = new REST({version: '10'}).setToken(process.env.BOT_TOKEN)
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON)
 const client = new Client({ intents: [
 	GatewayIntentBits.Guilds, 
